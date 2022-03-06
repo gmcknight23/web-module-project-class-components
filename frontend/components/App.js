5f0104732f84a2b384970adfa3725b59fc9efb65
gregmcknight <gmcknight23@gmail.com>
2022-03-06 15:41:12 -0500

    0341p

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import TodoList from "./TodoList";
+import Form from "./Form";
 
 export default class App extends React.Component {
   constructor() {
@@ -26,10 +27,7 @@ export default class App extends React.Component {
       <div>
         <h1>TODOS</h1>
         <TodoList todos={todos} />
-        <form>
-          <input />
-          <button>Add</button>
-        </form>
+        <Form todos={todos} />
         <button>Clear</button>
       </div>
     );
--- frontend/components/Form.js
+++ frontend/components/Form.js
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from "react";
 
 export default class Form extends React.Component {
   render() {
     return (
-      <div>
-        Form
-      </div>
-    )
+      <form>
+        <input />
+        <button>Add</button>
+      </form>
+    );
   }
 }
