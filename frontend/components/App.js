1f1434c9e713c2c76a9077b06373c71291f844bb
gregmcknight <gmcknight23@gmail.com>
2022-03-06 12:48:29 -0500

    1248pm

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,11 +1,21 @@
-import React from 'react'
+import React from "react";
 
 export default class App extends React.Component {
   render() {
     return (
       <div>
-        Todo App
+        <h1>Todo App</h1>
+        <ul>
+          <li>Walk the dog</li>
+          <li>Cook Dinner</li>
+          <li>Go to Gym</li>
+        </ul>
+        <form>
+          <input />
+          <button>Add</button>
+          <button>Clear</button>
+        </form>
       </div>
-    )
+    );
   }
 }
